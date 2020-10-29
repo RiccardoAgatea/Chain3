8ee63f0acfdfd3fe4cf344f5d4b2c00dfed2c072
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-10-29 21:02:19 +0100

    implement animation for tile collection

--- scripts/game-elements/Tile.gd
+++ scripts/game-elements/Tile.gd
@@ -16,10 +16,25 @@ func slide():
 
 func move_to(target):
 	$Tween.interpolate_property(
-		self, "position", position, target, 0.4, Tween.TRANS_BOUNCE, Tween.EASE_OUT
+		self, "position", position, target, 0.6, Tween.TRANS_BOUNCE, Tween.EASE_OUT
 	)
 	$Tween.start()
 
 
 func resource_name() -> String:
 	return info.name
+
+
+func collect(inventory_position):
+	$Tween.connect(
+		"tween_all_completed", self, "_on_Tween_tween_all_completed", [], CONNECT_ONESHOT
+	)
+	$Tween.interpolate_property(
+		self, "position", position, inventory_position, 0.3, Tween.TRANS_QUINT, Tween.EASE_IN_OUT
+	)
+	$Tween.start()
+	return info["resources"].duplicate(true)
+
+
+func _on_Tween_tween_all_completed():
+	queue_free()

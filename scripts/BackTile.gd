3943203b542b8c5f7a5b98e546a0aab709cfc669
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-12-05 16:32:51 +0100

    setup backtile functionality

--- scripts/BackTile.gd
+++ scripts/BackTile.gd
@@ -1,3 +1,30 @@
 extends Sprite
 
 class_name BackTile
+
+signal destroyed
+
+var full := false
+var stages: Array
+var current_stage: int
+
+
+func init(info: Dictionary):
+	full = true
+	stages = info.stages
+	set_texture(load(stages[0].sprite))
+	current_stage = 0
+
+
+func hit():
+	if not full:
+		return
+
+	current_stage += 1
+	if current_stage >= stages.size():
+		full = false
+		set_texture(null)
+		hide()
+		emit_signal("destroyed")
+	else:
+		set_texture(stages[current_stage])
